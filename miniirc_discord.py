3f3dbbd90122297eacc10b157c222a8f1000c2b0
luk3yx <luk3yx@users.noreply.github.com>
2019-01-11 17:52:58 +1300

    Make NOTICEs Discord Embeds  • Change NOTICE so it adds Discord embeds  • Release 0.5.0  • Make the irc.quote() handler nicer to read and use at-rules internally

--- miniirc_discord.py
+++ miniirc_discord.py
@@ -9,8 +9,8 @@
 
 import asyncio, discord, miniirc, re, time
 
-ver      = (0,4,1)
-version  = '0.4.1'
+ver      = (0,5,0)
+version  = '0.5.0'
 __all__  = ['Discord', 'miniirc']
 channels = {}
 
@@ -59,6 +59,85 @@ def _irc_to_discord(msg):
         msg = msg.replace(format + format, '').replace(format, formats[format])
     return msg
 
+# Register outgoing commands
+_outgoing_cmds = {}
+def _register_cmd(*cmds):
+    def x(func):
+        for cmd in cmds:
+            cmd = str(cmd).upper()
+            _outgoing_cmds[cmd] = func
+        return func
+    return x
+
+# PRIVMSG
+@_register_cmd('PRIVMSG')
+def _on_privmsg(self, client, run, tags, cmd, args):
+    if len(args) == 2 and args[0] in channels:
+        chan = channels[args[0]]
+        msg  = args[-1][1:]
+        if msg[:7].upper() == '\x01ACTION':
+            msg = '\x1d' + msg[8:].replace('\x01', '') + '\x1d'
+        msg = _irc_to_discord(msg)
+        self.debug('Translated PRIVMSG:', msg)
+        run(client.send_message(chan, msg))
+    else:
+        self.debug('Invalid call to PRIVMSG.')
+
+# IRC colour hex values
+_colours = [
+    0xffffff,   # 0.  White
+    0x000000,   # 1.  Black
+    0x0000ff,   # 2.  Blue
+    0x00ff00,   # 3.  Green
+    0xff0000,   # 4.  Red
+    0xd2691e,   # 5.  Brown
+    0xff00ff,   # 6.  Purple
+    0xff8800,   # 7.  Orange
+    0xffff00,   # 8.  Yellow
+    0x88ff88,   # 9.  Light green
+    # TODO: Add more and allow them to be used in NOTICE
+]
+
+# NOTICE
+@_register_cmd('NOTICE')
+def _on_notice(self, client, run, tags, cmd, args):
+    if len(args) != 2 or args[0] not in channels:
+        return self.debug('Invalid call to NOTICE.')
+    title = tags.get('+discordapp.com/embed-title') or ''
+    msg = args[1][1:]
+    colour = None
+    if msg.startswith('\x03') and len(msg) > 2:
+        try:
+            colour = _colours[int(msg[1])]
+        except:
+            pass
+    title, msg = _irc_to_discord(title), _irc_to_discord(msg)
+    embed = discord.Embed(title = title or None, description = msg,
+        colour = colour or discord.Embed.Empty)
+    run(client.send_message(channels[args[0]], embed = embed))
+
+# AWAY
+@_register_cmd('AWAY')
+def _on_away(self, client, run, tags, cmd, args):
+    game = ' '.join(args)
+    if game.startswith(':'):
+        game = game[1:]
+    ptype = (tags and tags.get('+discordapp.com/type') or '').lower()
+    url   = None
+    if ptype == 'watching':
+        ptype = 3
+    elif ptype == 'listening to':
+        ptype = 2
+    elif ptype == 'streaming':
+        ptype = 1
+        url  = 'https://www.twitch.tv/directory'
+    else:
+        ptype = 0
+    game = discord.Game(name = game, type = ptype, url = url)
+    self.debug('Changing online presence:', game)
+    run(client.change_presence(game = game))
+
+# The discord class
 class Discord(miniirc.IRC):
     _client = None
 
@@ -74,37 +153,12 @@ class Discord(miniirc.IRC):
         del _
 
         if type(tags) != dict:
-            tags = None
+            tags = {}
 
-        if cmd in ('PRIVMSG', 'NOTICE'):
-            if len(args) == 2 and args[0] in channels:
-                chan = channels[args[0]]
-                msg  = args[-1][1:]
-                if msg[:7].upper() == '\x01ACTION':
-                    msg = '\x1d' + msg[8:].replace('\x01', '') + '\x1d'
-                msg = _irc_to_discord(msg)
-                self.debug('Translated PRIVMSG:', msg)
-                self._run(self._client.send_message(chan, msg))
-            else:
-                self.debug('Invalid call to PRIVMSG.')
-        elif cmd == 'AWAY':
-            game = ' '.join(args)
-            if game.startswith(':'):
-                game = game[1:]
-            ptype = (tags and tags.get('+discordapp.com/type') or '').lower()
-            url   = None
-            if ptype == 'watching':
-                ptype = 3
-            elif ptype == 'listening to':
-                ptype = 2
-            elif ptype == 'streaming':
-                ptype = 1
-                url  = 'https://www.twitch.tv/directory'
-            else:
-                ptype = 0
-            game = discord.Game(name = game, type = ptype, url = url)
-            self.debug('Changing online presence:', game)
-            self._run(self._client.change_presence(game = game))
+        if _outgoing_cmds.get(cmd):
+            _outgoing_cmds[cmd](self, self._client, self._run, tags, cmd, args)
+        else:
+            self.debug('Unknown command run:', cmd)
 
     def _main(self):
         self.debug('Main loop running!')
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name        = 'miniirc_discord',
-    version     = '0.4.1',
+    version     = '0.5.0',
     py_modules  = ['miniirc_discord'],
     author      = 'luk3yx',
     description = 'A Discord wrapper for miniiric.',
